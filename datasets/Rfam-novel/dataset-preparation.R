6e402ea88ef0c41249a2e4c783aa70f7d32af3e8
Luigi Cerulo <luigi.cerulo@gmail.com>
2020-03-04 15:17:32 +0100

    update dataset

--- datasets/Rfam-novel/dataset-preparation.R
+++ datasets/Rfam-novel/dataset-preparation.R
@@ -1,14 +1,13 @@
-
-
-
 require(Biostrings)
 
 # downloaded 199110 sequences from Rfam database splitted in train, val and test sets
 load("train_test_val_41_Rfam_families.RDa",verbose = T)
 
-length(x_train)+length(x_val)+length(x_test)
+cat("Train: ",length(x_train),"\n")
+cat("Val: ",length(x_val),"\n")
+cat("Test: ",length(x_test),"\n")
 
-# remove sequences with letters different from canonical A T C and G
+cat("Remove sequences with letters different from canonical A T C and G\n")
 ca=vcountPattern('A',x_train)
 ct=vcountPattern('T',x_train)
 cc=vcountPattern('C',x_train)
@@ -27,35 +26,44 @@ cc=vcountPattern('C',x_test)
 cg=vcountPattern('G',x_test)
 w=ca+ct+cc+cg
 x_test=x_test[w==width(x_test)]
-length(x_train)+length(x_val)+length(x_test)
 
-# remove outliers
-length(x_train)
-table(names(x_train))
+cat("Train: ",length(x_train),"\n")
+cat("Val: ",length(x_val),"\n")
+cat("Test: ",length(x_test),"\n")
+
+cat("Remove outliers\n")
 minlen = 50
 maxlen = 150
 x_train=x_train[width(x_train)<=maxlen & width(x_train)>=minlen]
 x_val=x_val[width(x_val)<=maxlen & width(x_val)>=minlen]
 x_test=x_test[width(x_test)<=maxlen & width(x_test)>=minlen]
-length(x_train)+length(x_val)+length(x_test)
 
-#exclude classes with less that 400 samples
+cat("Train: ",length(x_train),"\n")
+cat("Val: ",length(x_val),"\n")
+cat("Test: ",length(x_test),"\n")
+
+
+cat("Exclude classes with less that 400 samples\n")
 # the final set is of 168278 sequences distributed among 30 different Rfam classes 
 
-length(x_train)
 x=table(names(x_train))
 classi = names(x[x>=400])
-length(classi)
 
+cat("Number of classes: ",length(classi),"\n")
+
+cat("Remove classes not in val and test set\n")
 cc = intersect(unique(names(x_val)),unique(names(x_test)))
 classi = intersect(cc,classi)
-length(classi)
+cat("Number of classes: ",length(classi),"\n")
 
 
 x_train = x_train[names(x_train) %in% classi]
 x_val = x_val[names(x_val) %in% classi]
 x_test = x_test[names(x_test) %in% classi]
-length(x_train)+length(x_val)+length(x_test)
+
+cat("Train: ",length(x_train),"\n")
+cat("Val: ",length(x_val),"\n")
+cat("Test: ",length(x_test),"\n")
 
 
 # figure distribution among 30 different Rfam classes
@@ -66,9 +74,7 @@ ggplot(data=df, aes(x=Class, y=Samples)) + xlab("") + ylab("# of samples")+
   geom_bar(stat="identity", width=0.5)+theme(axis.text.x = element_text(angle = 90, hjust = 1),text = element_text(size=16))
 ggsave("class-distribution.pdf",height = 6 , width = 10)
 
-
-# balancing of training/validation sets
-length(x_train)
+cat("Balancing of training/validation sets\n")
 minsize = min(table(names(x_train)))
 minsize = minsize*3
 x_train_new = DNAStringSet()
@@ -83,16 +89,12 @@ for (i in classi) {
   }
   x_val_new = c(x_val_new,a)
 }
-table(names(x_train_new))
-length(x_train_new)
+cat("Train: ",length(x_train_new),"\n")
+cat("Val: ",length(x_val_new),"\n")
+
 writeXStringSet(x_train_new,"x_train.fasta")
-table(names(x_val_new))
-length(x_val_new)
 writeXStringSet(x_val_new,"x_val.fasta")
 
-length(x_test)
 x_test = x_test[names(x_test) %in% classi]
-length(x_test)
-table(names(x_test))
 writeXStringSet(x_test,"x_test.fasta")
 
