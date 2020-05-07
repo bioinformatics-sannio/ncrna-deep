f53a42f68c0f9cc5bc87671652694738147479a4
Luigi Cerulo <lcerulo@unisannio.it>
2020-05-07 15:36:56 +0000

    moved py scripts

--- utils/ExpConfiguration.py
+++ utils/ExpConfiguration.py
@@ -9,7 +9,7 @@ fastaTest = 'datasets/Rfam-novel/x_test.fasta'
 
 kappas=[1,2,6]  # genome simulation multipliers (not used)
 nlayers=[0,1,2,3]  # deep architecture layers
-bnoise=[0,25,50,75,100] # boundary noise simulation
+bnoise=[0,25,50,75,100,125,150,175,200] # boundary noise simulation
 padds = ['random','constant','new'] # how padding is done
 
 # Encoders configurations 
@@ -20,7 +20,11 @@ seqEncoders = (
                 'param25' : [[64],3,['A','T','C','G']],
                 'param50' : [[75],3,['A','T','C','G']],
                 'param75' : [[88],3,['A','T','C','G']],
-                'param100' : [[100],3,['A','T','C','G']]
+                'param100' : [[100],3,['A','T','C','G']],
+                'param125' : [[113],3,['A','T','C','G']],
+                'param150' : [[125],3,['A','T','C','G']],
+                'param175' : [[138],3,['A','T','C','G']],
+                'param200' : [[150],3,['A','T','C','G']]
                           },
                {'enc' : seq2Kmer,
                 'filename' : '2mer',
@@ -28,7 +32,11 @@ seqEncoders = (
                 'param25' : [[95],2,['A','T','C','G']],
                 'param50' : [[113],2,['A','T','C','G']],
                 'param75' : [[132],2,['A','T','C','G']],
-                'param100' : [[150],2,['A','T','C','G']]
+                'param100' : [[150],2,['A','T','C','G']],
+                'param125' : [[169],3,['A','T','C','G']],
+                'param150' : [[188],3,['A','T','C','G']],
+                'param175' : [[207],3,['A','T','C','G']],
+                'param200' : [[225],3,['A','T','C','G']]
                           },
                {'enc' : seq2Kmer,
                 'filename' : '1mer',
@@ -36,7 +44,11 @@ seqEncoders = (
                 'param25' : [[190],1,['A','T','C','G']],
                 'param50' : [[225],1,['A','T','C','G']],
                 'param75' : [[263],1,['A','T','C','G']],
-                'param100' : [[300],1,['A','T','C','G']]
+                'param100' : [[300],1,['A','T','C','G']],
+                'param125' : [[338],3,['A','T','C','G']],
+                'param150' : [[375],3,['A','T','C','G']],
+                'param175' : [[413],3,['A','T','C','G']],
+                'param200' : [[450],3,['A','T','C','G']]
                           },               
                {'enc' : seq2Snake,
                 'filename' : 'Snake',
@@ -44,7 +56,11 @@ seqEncoders = (
                 'param25' : [[14,14],['A','T','C','G']],
                 'param50' : [[15,15],['A','T','C','G']],
                 'param75' : [[17,17],['A','T','C','G']],
-                'param100' : [[18,18],['A','T','C','G']]
+                'param100' : [[18,18],['A','T','C','G']],
+                'param125' : [[19,19],['A','T','C','G']],
+                'param150' : [[20,20],['A','T','C','G']],
+                'param175' : [[21,21],['A','T','C','G']],
+                'param200' : [[22,22],['A','T','C','G']]
                           },
                {'enc' : seq2Morton,
                 'filename' : 'Morton',
@@ -52,7 +68,11 @@ seqEncoders = (
                 'param25' : [[16,16],['A','T','C','G']],
                 'param50' : [[16,16],['A','T','C','G']],
                 'param75' : [[32,32],['A','T','C','G']],
-                'param100' : [[32,32],['A','T','C','G']]
+                'param100' : [[32,32],['A','T','C','G']],
+                'param125' : [[32,32],['A','T','C','G']],
+                'param150' : [[32,32],['A','T','C','G']],
+                'param175' : [[32,32],['A','T','C','G']],
+                'param200' : [[32,32],['A','T','C','G']]
                           },
                {'enc' : seq2Hilbert,
                 'filename' : 'Hilbert',
@@ -60,7 +80,11 @@ seqEncoders = (
                 'param25' : [[16,16],['A','T','C','G']],
                 'param50' : [[16,16],['A','T','C','G']],
                 'param75' : [[32,32],['A','T','C','G']],
-                'param100' : [[32,32],['A','T','C','G']]
+                'param100' : [[32,32],['A','T','C','G']],
+                'param125' : [[32,32],['A','T','C','G']],
+                'param150' : [[32,32],['A','T','C','G']],
+                'param175' : [[32,32],['A','T','C','G']],
+                'param200' : [[32,32],['A','T','C','G']]
                           }
                )
 
